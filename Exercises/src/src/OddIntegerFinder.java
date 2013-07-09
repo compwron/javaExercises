aaaa7203ddaa08d23ea48e1958cf74af77c3d66a
compwron <wordymusic@gmail.com>
2013-07-08 23:27:29 -0500

    failing test for simplest (only contains one odd integer) OddIntegerFinder

---
+++ Exercises/src/src/OddIntegerFinder.java
@@ -0,0 +1,12 @@
+package src;
+
+import java.util.ArrayList;
+
+public class OddIntegerFinder {
+    public OddIntegerFinder(ArrayList<Integer> integers) {
+    }
+
+    public ArrayList<Integer> odds() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+}
---
+++ Exercises/test/OddIntegerFinderTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import src.OddIntegerFinder;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class OddIntegerFinderTest {
+    @Test
+    public void findsOneOddInteger(){
+        assertThat(new OddIntegerFinder(arrayContaining(1)).odds(), is(arrayContaining(1)));
+    }
+
+    private ArrayList<Integer> arrayContaining(Integer... integers){
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        for (Integer integer : integers){
+            array.add(integer);
+        }
+        return array;
+    }
+}
