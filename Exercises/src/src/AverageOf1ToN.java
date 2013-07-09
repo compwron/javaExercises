18c4a21024753ac70000e05569df9f9dc59188b4
compwron <wordymusic@gmail.com>
2013-07-08 21:38:00 -0500

    failing test for average

--- Exercises/src/src/AverageOf1ToN.java
+++ Exercises/src/src/AverageOf1ToN.java
@@ -1,10 +1,13 @@
 package src;
 
 public class AverageOf1ToN {
+    private final Integer number;
+
     public AverageOf1ToN(Integer number) {
+        this.number = number;
     }
 
-    public Integer average() {
-        return 1;
+    public Double average() {
+        return 1.0;
     }
 }
