5a5cac5045aac1390cefaaec7cb1350b146d59d8
compwron <wordymusic@gmail.com>
2013-07-08 23:23:03 -0500

    using while instead of recursion for pi approximation in order to estimate more accurately without stack overflow

--- Exercises/src/src/ApproximatePi.java
+++ Exercises/src/src/ApproximatePi.java
@@ -6,25 +6,20 @@ public class ApproximatePi {
     public Double accuracy(int fractionAccuracy) {
         counterGoal = fractionAccuracy;
         Integer currentFractionDenominator = -3; // algorithm dictates that first denominator is 3
-        return 4 * (1 + fractions(0.0, currentFractionDenominator, 0)); // starts with fraction sum of 0
+        return 4 * (1 + fractions(0.0, currentFractionDenominator, 1)); // starts with fraction sum of 0
     }
 
     private Double fractions(Double currentFractionSum, Integer currentFractionDenominator, Integer counter){
-        currentFractionSum += (1.0 / currentFractionDenominator);
-        boolean wasPositive = currentFractionDenominator > 0;
-        currentFractionDenominator = Math.abs(currentFractionDenominator);
-        currentFractionDenominator += 2;
-        if (wasPositive){
-            currentFractionDenominator = currentFractionDenominator * -1;
+        while(counter <= counterGoal){
+            currentFractionSum += (1.0 / currentFractionDenominator);
+            boolean wasPositive = currentFractionDenominator > 0;
+            currentFractionDenominator = Math.abs(currentFractionDenominator);
+            currentFractionDenominator += 2;
+            if (wasPositive){
+                currentFractionDenominator = currentFractionDenominator * -1;
+            }
+            counter += 1;
         }
-        counter += 1;
-
-        if(counter == counterGoal){
-            return currentFractionSum;
-        } else {
-            return fractions(currentFractionSum, currentFractionDenominator, counter);
-        }
+        return currentFractionSum;
     }
-
-
 }
