59ce6f453bd46e3c0d9418c7fee9ddf2db77bec3
compwron <wordymusic@gmail.com>
2013-07-12 16:21:53 -0500

    first failing test for parsing text for markov

---
+++ Exercises/src/src/Markov.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Markov {
+    public Markov(String text) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public String generate() {
+        return "";
+    }
+
+    public Integer frequencyOf(String a) {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public String mostCommonlyFollowedBy(String a) {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+}
--- Exercises/test/MarkovTest.java
+++ Exercises/test/MarkovTest.java
@@ -1,2 +1,21 @@
+import org.junit.Test;
+import src.Markov;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class MarkovTest {
+
+    @Test
+    public void shouldReadFrequenciesFromText(){
+
+        // list of: letter => [[following letter => frequency], [following letter => frequency]]
+//        for(words in text.count); make_word
+        String text = "ab";
+        Markov chain = new Markov(text);
+        assertThat(chain.frequencyOf("a"), is(1));
+        assertThat(chain.mostCommonlyFollowedBy("a"), is("b"));
+
+    }
+
 }
