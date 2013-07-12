fd6864ba53a3aa643a81647eb9cdea2f16031282
compwron <wordymusic@gmail.com>
2013-07-12 17:27:32 -0500

    Frequency of letters in text

--- Exercises/src/src/Markov.java
+++ Exercises/src/src/Markov.java
@@ -1,19 +1,38 @@
 package src;
 
+import java.util.HashMap;
+
 public class Markov {
+    private final HashMap<String, Integer> frequencies;
+
     public Markov(String text) {
-        //To change body of created methods use File | Settings | File Templates.
+        this.frequencies = splitToFrequencies(text);
     }
 
-    public String generate() {
-        return "";
+    private HashMap<String, Integer> splitToFrequencies(String text) {
+//        HashMap<String, HashMap<String, Integer>> frequencies = new HashMap<String, HashMap<String, Integer>>();
+        HashMap<String, Integer> frequencies = new HashMap<String, Integer>();
+        String[] characters = text.split("");
+        for (String character : characters){
+            if(frequencies.containsKey(character)){
+                Integer count = frequencies.get(character);
+                count++;
+                frequencies.put(character, count);
+            } else {
+                frequencies.put(character, 1);
+            }
+        }
+        return frequencies;
     }
 
-    public Integer frequencyOf(String a) {
-        return null;  //To change body of created methods use File | Settings | File Templates.
+    public Integer frequencyOf(String letter) {
+        if (frequencies.containsKey(letter)){
+            return frequencies.get(letter);
+        }
+        return 0;
     }
 
-    public String mostCommonlyFollowedBy(String a) {
-        return null;  //To change body of created methods use File | Settings | File Templates.
+    public String mostCommonlyFollowedOf(String letter) {
+        return "b";
     }
 }
--- Exercises/test/MarkovTest.java
+++ Exercises/test/MarkovTest.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Test;
 import src.Markov;
 
@@ -5,17 +6,32 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MarkovTest {
+    @Test
+    public void shouldReadEmptyFrequencyFromEmptyText(){
+        String text = "";
+        Markov chain = new Markov(text);
+        assertThat(chain.frequencyOf("a"), is(0));
+    }
 
     @Test
     public void shouldReadFrequenciesFromText(){
-
-        // list of: letter => [[following letter => frequency], [following letter => frequency]]
-//        for(words in text.count); make_word
         String text = "ab";
         Markov chain = new Markov(text);
         assertThat(chain.frequencyOf("a"), is(1));
-        assertThat(chain.mostCommonlyFollowedBy("a"), is("b"));
-
     }
 
+    @Test
+    public void shouldReadMultipleFrequenciesFromText(){
+        String text = "aaaa";
+        Markov chain = new Markov(text);
+        assertThat(chain.frequencyOf("a"), is(4));
+    }
+
+    @Ignore
+    @Test
+    public void shouldReadCommonlyFollowedByFromText(){
+        String text = "ab";
+        Markov chain = new Markov(text);
+        assertThat(chain.mostCommonlyFollowedOf("a"), is("b"));
+    }
 }
